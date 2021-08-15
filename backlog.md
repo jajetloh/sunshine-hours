c33cdfe079fdd51113405aefbf8015ed800ad682
jajet.loh <jajetloh@gmail.com>
2021-08-15 17:31:45 +0800

    basic streamlit app working; added function to populate data with lat/long coordinates

---
+++ backlog.md
@@ -0,0 +1,21 @@
+# Current tasks
+
+- Figure out what chart type to use. Scatter is a bit basic. See [pydeck Layers](https://deckgl.readthedocs.io/en/latest/index.html).
+
+# Reminders
+
+## How to run app locally
+Activate virtual environment with <br>
+``source sunshine-env/bin/activate`` <br>
+Then run app with <br>
+``streamlit run main-app.py``
+
+## Saving/installing current pip dependencies
+``pip freeze requirements.txt`` <br>
+``pip install -r requirements.txt``
+
+
+# Useful links
+- [Streamlit Documentation](https://docs.streamlit.io/en/stable/api.html)
+- [Deploying Streamlit on share.streamlit.io](https://docs.streamlit.io/en/stable/deploy_streamlit_app.html#log-in-to-share-streamlit-io)
+- [pydeck Layers](https://deckgl.readthedocs.io/en/latest/index.html)
--- main-app.py
+++ main-app.py
@@ -11,64 +11,48 @@ def from_data_file(filename):
         "example-data/master/hello/v1/%s" % filename)
     return pd.read_json(url)
 
-try:
-    ALL_LAYERS = {
-        "Bike Rentals": pdk.Layer(
-            "HexagonLayer",
-            data=from_data_file("bike_rental_stats.json"),
-            get_position=["lon", "lat"],
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
-            extruded=True,
-        ),
-        "Bart Stop Exits": pdk.Layer(
-            "ScatterplotLayer",
-            data=from_data_file("bart_stop_stats.json"),
-            get_position=["lon", "lat"],
-            get_color=[200, 30, 0, 160],
-            get_radius="[exits]",
-            radius_scale=0.05,
-        ),
-        "Bart Stop Names": pdk.Layer(
-            "TextLayer",
-            data=from_data_file("bart_stop_stats.json"),
-            get_position=["lon", "lat"],
-            get_text="name",
-            get_color=[0, 0, 0, 200],
-            get_size=15,
-            get_alignment_baseline="'bottom'",
-        ),
-        "Outbound Flow": pdk.Layer(
-            "ArcLayer",
-            data=from_data_file("bart_path_stats.json"),
-            get_source_position=["lon", "lat"],
-            get_target_position=["lon2", "lat2"],
-            get_source_color=[200, 30, 0, 160],
-            get_target_color=[200, 30, 0, 160],
-            auto_highlight=True,
-            width_scale=0.0001,
-            get_width="outbound",
-            width_min_pixels=3,
-            width_max_pixels=30,
-        ),
-    }
-    st.sidebar.markdown('### Map Layers')
-    selected_layers = [
-        layer for layer_name, layer in ALL_LAYERS.items()
-        if st.sidebar.checkbox(layer_name, True)]
-    if selected_layers:
-        st.pydeck_chart(pdk.Deck(
-            map_style="mapbox://styles/mapbox/light-v9",
-            initial_view_state={"latitude": 37.76,
-                                "longitude": -122.4, "zoom": 11, "pitch": 50},
-            layers=selected_layers,
-        ))
-    else:
-        st.error("Please choose at least one layer above.")
-except URLError as e:
-    st.error("""
-        **This demo requires internet access.**
 
-        Connection error: %s
-    """ % e.reason)
+
+if __name__ == '__main__':
+
+    df = pd.read_excel('CitiesBySunshineDurationWithCoordinates.xlsx')
+
+    try:
+        st.sidebar.markdown('### Map Layers')
+        selected_month = st.sidebar.select_slider('Month', options=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+        ALL_LAYERS = {
+            "My Layer": pdk.Layer(
+                "ScatterplotLayer",
+                data=df,
+                pickable=True,
+                opacity=0.8,
+                stroked=True,
+                filled=True,
+                radius_scale=60,
+                radius_min_pixels=1,
+                radius_max_pixels=10,
+                line_width_min_pixels=1,
+                get_position=['Longitude', 'Latitude'],
+                get_radius=selected_month,
+                get_fill_color=[255, 255, 0],
+                get_line_color=[0, 0, 0],
+            )
+        }
+        selected_layers = [
+            layer for layer_name, layer in ALL_LAYERS.items()
+            if st.sidebar.checkbox(layer_name, True)]
+        if selected_layers:
+            st.pydeck_chart(pdk.Deck(
+                map_style="mapbox://styles/mapbox/light-v9",
+                initial_view_state={"latitude": 0,
+                                    "longitude": -122.4, "zoom": 0, "pitch": 0},
+                layers=selected_layers,
+            ))
+        else:
+            st.error("Please choose at least one layer above.")
+    except URLError as e:
+        st.error("""
+            **This demo requires internet access.**
+
+            Connection error: %s
+        """ % e.reason)
