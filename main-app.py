5ceec5e749bed246b438849f698b9205060eac73
jajet.loh <jajetloh@gmail.com>
2021-08-23 22:15:13 +0800

    add column layer and linear colour interpolation

--- main-app.py
+++ main-app.py
@@ -4,49 +4,69 @@ import pydeck as pdk
 
 from urllib.error import URLError
 
-@st.cache
-def from_data_file(filename):
-    url = (
-        "https://raw.githubusercontent.com/streamlit/"
-        "example-data/master/hello/v1/%s" % filename)
-    return pd.read_json(url)
+def hex_to_rgb(h):
+    '''Takes a hex rgb string (e.g. #ffffff) and returns an RGB tuple (float, float, float).'''
+    return tuple(int(h[i:i + 2], 16) for i in (1, 3, 5))
 
-
+class LinearColourInterpolator:
+    interpolate = None
+    def __init__(self, rgb_min, rgb_max, n_min, n_max):
+        def value_to_rgb(value):
+            rgb1 = rgb_min
+            rgb2 = rgb_max
+            if value <= n_min:
+                return rgb1
+            elif value >= n_max:
+                return rgb2
+            return [i1 + (i2 - i1) * (value - n_min)/(n_max - n_min) for i1, i2 in zip(rgb1, rgb2)]
+        self.interpolate = value_to_rgb
 
 if __name__ == '__main__':
 
     df = pd.read_excel('CitiesBySunshineDurationWithCoordinates.xlsx')
+    df_melt = pd.melt(df, id_vars=['Continent', 'Country', 'City', 'Latitude', 'Longitude'], value_vars=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], var_name='Month', value_name='Sunshine')
+
+    # Min and max values found in all individual months
+    n_min = min(df_melt['Sunshine'].values)
+    n_max = max(df_melt['Sunshine'].values)
 
     try:
-        st.sidebar.markdown('### Map Layers')
+        st.sidebar.markdown('### Month Selection')
         selected_month = st.sidebar.select_slider('Month', options=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+        
+        st.sidebar.markdown('### Colours')
+
+        selected_min_colour = st.sidebar.color_picker('Min Colour')
+        selected_max_colour = st.sidebar.color_picker('Max Colour')
+
+        lci = LinearColourInterpolator(hex_to_rgb(selected_min_colour), hex_to_rgb(selected_max_colour), n_min, n_max)
+        df_melt['Colour'] = [lci.interpolate(v) for v in df_melt['Sunshine'].values]
+
         ALL_LAYERS = {
-            "My Layer": pdk.Layer(
-                "ScatterplotLayer",
-                data=df,
-                pickable=True,
-                opacity=0.8,
-                stroked=True,
-                filled=True,
-                radius_scale=500,
-                # radius_scale=60,
-                radius_min_pixels=2,
-                radius_max_pixels=60,
-                line_width_min_pixels=1,
+            "Sunshine Hours": pdk.Layer(
+                "ColumnLayer",
+                data=df_melt[df_melt['Month'] == selected_month],
                 get_position=['Longitude', 'Latitude'],
-                get_radius=selected_month,
-                get_fill_color=[255, 255, 0],
-                get_line_color=[0, 0, 0],
-            )
+                get_elevation='Sunshine',
+                elevation_scale=2e4,
+                radius=1e5,
+                pickable=True,
+                auto_highlight=True,
+                opacity=0.7,
+                get_fill_color='Colour',
+            ),
         }
+
+        st.sidebar.markdown('### Map Layers')
         selected_layers = [
             layer for layer_name, layer in ALL_LAYERS.items()
             if st.sidebar.checkbox(layer_name, True)]
+
         if selected_layers:
             st.pydeck_chart(pdk.Deck(
                 map_style="mapbox://styles/mapbox/light-v9",
                 initial_view_state={"latitude": 0,
-                                    "longitude": -122.4, "zoom": 0, "pitch": 0},
+                                    "longitude": -122.4, "zoom": 0.1, "pitch": 10},
                 layers=selected_layers,
                 tooltip={"text": "{City}, {Country}\n" + selected_month + ': {' + selected_month + '}'}
             ))
